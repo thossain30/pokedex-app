604c07455293cc815e80e473cf9d297d2a784b27
thossain30 <54952959+thossain30@users.noreply.github.com>
2025-09-08 12:25:03 -0400

    Added endpoint to retrieve all pokemon

---
+++ backend/pokedex/src/main/java/com/pokedex/app/Constants.java
@@ -0,0 +1,8 @@
+package com.pokedex.app;
+
+public class Constants {
+    public static final String GEN_1_LIMIT = "151";
+    public static final String GEN_2_LIMIT = "251";
+    public static final String GEN_3_LIMIT = "386";
+    public static final String GEN_4_LIMIT = "493";
+}
--- backend/pokedex/src/main/java/com/pokedex/app/PokeApiController.java
+++ backend/pokedex/src/main/java/com/pokedex/app/PokeApiController.java
@@ -4,27 +4,40 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping(ApiEndpoints.BASE_API)
 public class PokeApiController {
     private final RestTemplate restTemplate = new RestTemplate();
 
+    // Returns all pokemon based on limit (doing up to end of gen 4 for now)
+    @GetMapping(ApiEndpoints.POKEMON)
+    public ResponseEntity<String> getAllPokemon(@RequestParam(defaultValue = Constants.GEN_4_LIMIT) int limit) {
+        String url = UriComponentsBuilder
+            .fromHttpUrl(ApiEndpoints.POKEMON)
+            .queryParam("limit", limit)
+            .toUriString();
+        String response = restTemplate.getForObject(url, String.class);
+        return ResponseEntity.ok(response);
+    }
+
+    // Returns 1 pokemon based on name
     @GetMapping(ApiEndpoints.POKEMON + "/{name}")
     public ResponseEntity<String> getPokemon(@PathVariable String name) {
-        // Call the PokeAPI
         String url = ApiEndpoints.POKEMON + name.toLowerCase();
         String response = restTemplate.getForObject(url, String.class);
         return ResponseEntity.ok(response);
     }
 
+    // Returns type based on number
     @GetMapping(ApiEndpoints.TYPE + "{num}")
     public ResponseEntity<String> getMethodName(@PathVariable String num) {
         String url = ApiEndpoints.TYPE + num.toLowerCase();
         String response = restTemplate.getForObject(url, String.class);
         return ResponseEntity.ok(response);
     }
-    
 }
