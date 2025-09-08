48c99d695bfbb0a37043b4bbce49708f2bcb9a36
thossain30 <54952959+thossain30@users.noreply.github.com>
2025-09-07 23:36:36 -0400

    added endpoint to retrieve type information

--- backend/pokedex/src/main/java/com/pokedex/app/ApiEndpoints.java
+++ backend/pokedex/src/main/java/com/pokedex/app/ApiEndpoints.java
@@ -2,5 +2,6 @@ package com.pokedex.app;
 
 public final class ApiEndpoints {
     public static final String BASE_API = "https://pokeapi.co/api/v2/";
-    public static final String POKEMON = BASE_API + "/pokemon";
+    public static final String POKEMON = BASE_API + "pokemon";
+    public static final String TYPE = BASE_API + "type";
 }
--- backend/pokedex/src/main/java/com/pokedex/app/PokeApiController.java
+++ backend/pokedex/src/main/java/com/pokedex/app/PokeApiController.java
@@ -1,5 +1,30 @@
 package com.pokedex.app;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping(ApiEndpoints.BASE_API)
 public class PokeApiController {
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    @GetMapping(ApiEndpoints.POKEMON + "/{name}")
+    public ResponseEntity<String> getPokemon(@PathVariable String name) {
+        // Call the PokeAPI
+        String url = ApiEndpoints.POKEMON + name.toLowerCase();
+        String response = restTemplate.getForObject(url, String.class);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping(ApiEndpoints.TYPE + "{num}")
+    public ResponseEntity<String> getMethodName(@PathVariable String num) {
+        String url = ApiEndpoints.TYPE + num.toLowerCase();
+        String response = restTemplate.getForObject(url, String.class);
+        return ResponseEntity.ok(response);
+    }
     
 }
